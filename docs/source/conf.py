7e5d10fa93e9869d4c67195e3ad746f782e7ad2a
Armando Herrera <anhydrous99@gmail.com>
2019-07-01 20:46:10 -0500

    Setting release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Armando Herrera'
 author = 'Armando Herrera'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
